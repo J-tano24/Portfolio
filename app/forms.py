edeeb87c96d58c9529b076fd952688c540f77cdd
jtano <j.tanoue24@gmail.com>
2021-02-06 12:27:27 +0900

    password reset template rivise

--- app/forms.py
+++ app/forms.py
@@ -39,18 +39,21 @@ class MyPasswordChangeForm(PasswordChangeForm):
     super().__init__(*args, **kwargs)
     for field in self.fields.values():
       field.widget.attrs['class'] = 'form-control'
+      field.widget.attrs['placeholder'] = field.label
 
 class MyPasswordResetForm(PasswordResetForm):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     for field in self.fields.values():
       field.widget.attrs['class'] = 'form-control'
+      field.widget.attrs['placeholder'] = field.label
 
 class MySetPasswordForm(SetPasswordForm):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     for field in self.fields.values():
       field.widget.attrs['class'] = 'form-control'
+      field.widget.attrs['placeholder'] = field.label
 
 class PhotoForm(ModelForm):
   class Meta:
