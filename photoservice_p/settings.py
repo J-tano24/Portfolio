976449c05ae14a735436346478ee79f292f8c84c
jtano <j.tanoue24@gmail.com>
2021-02-14 19:45:19 +0900

    Setting DEBUG, ALLOWED_HOST for development

--- photoservice_p/settings.py
+++ photoservice_p/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'p1mp-=ipk@g%u13mfw7_qu9c$4g^fc2xc!!udzqd_eph_ene-e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = "True"
+# DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['photoservice-p.herokuapp.com']
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['photoservice-p.herokuapp.com']
 
 
 # Application definition
